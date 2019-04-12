5e2915963976beb7f43efb7d9476ebd9b98ba127
luomh <tfinfo168518>
2019-04-12 15:35:58 +0800

    MVPHost 代码调整 20190412 updateBy luomh

--- src/main/java/cn/tzauto/octopus/secsLayer/equipImpl/mvp/MVPHost.java
+++ src/main/java/cn/tzauto/octopus/secsLayer/equipImpl/mvp/MVPHost.java
@@ -40,6 +40,8 @@ public class MVPHost extends EquipHost {
         ecFormat = FormatCode.SECS_4BYTE_UNSIGNED_INTEGER;
         ceFormat = FormatCode.SECS_4BYTE_UNSIGNED_INTEGER;
         rptFormat = FormatCode.SECS_4BYTE_UNSIGNED_INTEGER;
+        // TODO: 2019/4/12 StripMapUpCeid unknown ?
+//        StripMapUpCeid = ?
     }
 
 
@@ -87,23 +89,7 @@ public class MVPHost extends EquipHost {
                 if (msg.getMsgSfName() != null && msg.getMsgSfName().equalsIgnoreCase("s5f1in")) {
                     this.processS5F1in(msg);
                 } else if (msg.getMsgSfName() != null && msg.getMsgSfName().equalsIgnoreCase("s6f11in")) {
-                    long ceid = 0l;
-                    try {
-                        ceid = msg.getSingleNumber("CollEventID");
-                    } catch (Exception e) {
-                        logger.error("Exception:", e);
-                    }
-                    if (ceid == 3002) {
-                        //TODO 获取状态变化的事件报告
-                        processS6F11EquipStatusChange(msg);
-                    } else if (ceid == 3006 || ceid == 3007) {
-                        processS6F11EquipStatus(msg);
-                    } else if (ceid == 3008 || ceid == 3009 || ceid == 3010 || ceid == 3011 || ceid == 3012 || ceid == 3013) {
-                        //TODO 切换recipe后获取事件报告
-                        sendS1F3Check();
-                    }
-                } else if (msg.getMsgSfName().contains("s6f11inStripMapUpload")) {
-                    processS6F11inStripMapUpload(msg);
+                    processS6F11in(msg);
                 }
             } catch (InterruptedException e) {
                 logger.fatal("Caught Interruption", e);
@@ -125,7 +111,7 @@ public class MVPHost extends EquipHost {
             } else if (tagName.equalsIgnoreCase("s1f1in")) {
                 processS1F1in(data);
             } else if (tagName.equalsIgnoreCase("s6f11in")) {
-                processS6F11in(data);
+                this.inputMsgQueue.put(data);
             } else if (tagName.equalsIgnoreCase("s1f2in")) {
                 processS1F2in(data);
             } else if (tagName.equalsIgnoreCase("s1f14in")) {
@@ -145,10 +131,40 @@ public class MVPHost extends EquipHost {
     }
 
     // <editor-fold defaultstate="collapsed" desc="S6FX Code">
+
+    public void processS6F11in(DataMsgMap data) {
+        try {
+            long ceid = (long) data.get("CEID");
+            if (ceid == 3002) {
+                //TODO 获取状态变化的事件报告
+                processS6F11EquipStatusChange(data);
+            } else if (ceid == 3006 || ceid == 3007) {
+                processS6F11EquipStatus(data);
+            } else if (ceid == 3008 || ceid == 3009 || ceid == 3010 || ceid == 3011 || ceid == 3012 || ceid == 3013) {
+                //TODO 切换recipe后获取事件报告
+                findDeviceRecipe();
+//                sendS1F3Check();
+            } else if (ceid == StripMapUpCeid) {
+                // TODO: 2019/4/12  StripMapUpCeid unknown?
+                logger.info("----Received from Equip Strip Map Upload event - S6F11");
+                Long result = (Long) data.get("RESULT");
+                // 判断机台检测结果，如果为0则上传，结果为1 || 2则不上传
+                if (result == 0 || result == -1L) {
+                    processS6F11inStripMapUpload(data);
+                } else {
+                    logger.info("检测结果为:" + result + ",不上传mapping!");
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+        }
+
+    }
+
     protected void processS6F11EquipStatus(DataMsgMap data) {
         long ceid = 0l;
         try {
-            ceid = data.getSingleNumber("CollEventID");
+            ceid = (long) data.get("CEID");
             Map panelMap = new HashMap();
             if (ceid == 3006) {
                 panelMap.put("ControlState", FengCeConstant.CONTROL_LOCAL_ONLINE);       //Online_Local
@@ -162,178 +178,25 @@ public class MVPHost extends EquipHost {
         }
     }
 
-    @Override
-    protected void processS6F11inStripMapUpload(DataMsgMap data) {
-        logger.info("----Received from Equip Strip Map Upload event - S6F11");
-        Long result = -1L;
-        try {
-//            判断机台检测结果，如果为0则上传，结果为1 || 2则不上传
-            try {
-                result = data.getSingleNumber("RESULT");
-            } catch (Exception e) {
-                result = -1L;
-            }
-            if (result == 0 || result == -1L) {
-                //获取xml字符串
-                String stripMapData = (String) ((SecsItem) data.get("MapData")).getData();
-                String stripId = XmlUtil.getStripIdFromXml(stripMapData);
-                UiLogUtil.appendLog2SecsTab(deviceCode, "请求上传Strip Map！StripID:[" + stripId + "]");
-                //通过Web Service上传mapping
-                DataMsgMap out = new DataMsgMap("s6f12out", activeWrapper.getDeviceId());
-                byte[] ack = new byte[1];
-                ack[0] = WSUtility.binSet(stripMapData, deviceCode).getBytes()[0];
-                if (ack[0] == '0') {//上传成功
-                    ack[0] = 0;
-                    UiLogUtil.appendLog2SeverTab(deviceCode, "上传Strip Map成功！StripID:[" + stripId + "]");
-                } else {//上传失败
-                    ack[0] = 1;
-                    UiLogUtil.appendLog2SeverTab(deviceCode, "上传Strip Map失败！StripID:[" + stripId + "]");
-                }
-                out.put("AckCode", ack);
-                out.setTimeStamp(new Date());
-                out.setTransactionId(data.getTransactionId());
-                activeWrapper.respondMessage(out);
-                logger.info(" ----- s6f12 sended - Strip Upload Completed-----.");
-            } else {
-                logger.info("检测结果为:" + result + ",不上传mapping!");
-            }
-
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-    }
-
-    @Override
-    protected void processS6F11EquipStatusChange(DataMsgMap data) {
-        long ceid = 0l;
-        try {
-            ceid = data.getSingleNumber("CollEventID");
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-        //获取当前设备状态
-        sendS1F3Check();
-
-        SqlSession sqlSession = MybatisSqlSession.getSqlSession();
-        DeviceService deviceService = new DeviceService(sqlSession);
-        RecipeService recipeService = new RecipeService(sqlSession);
-
-        try {
-            //从数据库中获取当前设备模型信息
-            DeviceInfoExt deviceInfoExt = deviceService.getDeviceInfoExtByDeviceCode(deviceCode);
-            boolean dataReady = false;
-            //判断当前执行程序是否是清模程序 Y代表清模程序
-            boolean isCleanRecipe = false;
-            List<Recipe> cleanRecipes = recipeService.searchRecipeByRcpType(ppExecName, "Y");
-            if (cleanRecipes != null && cleanRecipes.size() > 1) {
-                isCleanRecipe = true;
-            }
-            // 更新设备模型
-            if (deviceInfoExt == null) {
-                logger.error("数据库中确少该设备模型配置；DEVICE_CODE:" + deviceCode);
-                UiLogUtil.appendLog2EventTab(deviceCode, "工控上不存在设备模型信息，不允许开机！请联系ME处理！");
-            } else {
-                deviceInfoExt.setDeviceStatus(equipStatus);
-                deviceService.modifyDeviceInfoExt(deviceInfoExt);
-                sqlSession.commit();
-                dataReady = true;
-            }
-
-            //保存到设备操作记录数据库
-            saveOplogAndSend2Server(ceid, deviceService, deviceInfoExt);
-            sqlSession.commit();
-
-            boolean checkResult = false;
-            //获取设备当前运行状态，如果是Run，执行开机检查逻辑
-            if (!isCleanRecipe && dataReady && equipStatus.equalsIgnoreCase("run")) {
-                //1、获取设备需要校验的信息类型,
-                String startCheckMod = deviceInfoExt.getStartCheckMod();
-                boolean hasGoldRecipe = true;
-                if (deviceInfoExt.getRecipeId() == null || "".equals(deviceInfoExt.getRecipeId())) {
-//                    holdDeviceAndShowDetailInfo();
-                    UiLogUtil.appendLog2EventTab(deviceCode, "Trackin数据不完整，未设置当前机台应该执行的Recipe，请改机!");
-                    return;
-                }
-                //查询trackin时的recipe和GoldRecipe
-                Recipe downLoadRecipe = recipeService.getRecipe(deviceInfoExt.getRecipeId());
-                List<Recipe> downLoadGoldRecipe = recipeService.searchRecipeGoldByPara(deviceInfoExt.getRecipeName(), deviceType, "GOLD", String.valueOf(deviceInfoExt.getVerNo()));
-
-                //查询客户端数据库是否存在GoldRecipe
-                if (downLoadGoldRecipe == null || downLoadGoldRecipe.isEmpty()) {
-                    hasGoldRecipe = false;
-                }
-
-                //首先从服务端获取机台是否处于锁机状态
-                //如果设备应该是锁机，那么首先发送锁机命令给机台
-                if (this.checkLockFlagFromServerByWS(deviceCode)) {
-                    UiLogUtil.appendLog2SeverTab(deviceCode, "检测到设备被设置为锁机，设备将被锁!");
-                    holdDeviceAndShowDetailInfo("Equipment has been set and locked by Server");
-                } else {
-                    //根据检查模式执行开机检查逻辑
-                    //1、A1-检查recipe名称是否一致
-                    //2、A-检查recipe名称和参数
-                    //3、B-检查SV
-                    //4、AB都检查
-                    if (startCheckMod != null && !"".equals(startCheckMod)) {
-                        checkResult = checkRecipeName(deviceInfoExt.getRecipeName());
-                        if (!checkResult) {
-                            UiLogUtil.appendLog2EventTab(deviceCode, "Recipe名称为：" + ppExecName + "，与改机后程序不一致，核对不通过，设备被锁定！请联系PE处理！");
-                            //不允许开机
-                            holdDeviceAndShowDetailInfo("RecipeName Error! Equipment locked!");
-                        } else {
-                            UiLogUtil.appendLog2EventTab(deviceCode, "Recipe名称为：" + ppExecName + "，与改机后程序一致，核对通过！");
-                        }
-                    }
-                    if (checkResult && "A".equals(startCheckMod)) {
-                        //首先判断下载的Recipe类型
-                        //1、如果下载的是Unique版本，那么执行完全比较
-                        String downloadRcpVersionType = downLoadRecipe.getVersionType();
-                        if ("Unique".equals(downloadRcpVersionType)) {
-                            UiLogUtil.appendLog2EventTab(deviceCode, "开始执行Recipe[" + ppExecName + "]参数绝对值Check");
-                            this.startCheckRecipePara(downLoadRecipe, "abs");
-                        } else {//2、如果下载的Gold版本，那么根据EXT中保存的版本号获取当时的Gold版本号，比较参数
-                            UiLogUtil.appendLog2EventTab(deviceCode, "开始执行Recipe[" + ppExecName + "]参数WICheck");
-                            if (!hasGoldRecipe) {
-                                UiLogUtil.appendLog2EventTab(deviceCode, "工控上不存在： " + ppExecName + " 的Gold版本，无法执行开机检查，设备被锁定！请联系PE处理！");
-                                //不允许开机
-                                this.holdDeviceAndShowDetailInfo("Host has no gold recipe, equipment locked!");
-                            } else {
-                                UiLogUtil.appendLog2EventTab(deviceCode, ppExecName + "开始WI参数Check");
-                                this.startCheckRecipePara(downLoadGoldRecipe.get(0));
-                            }
-                        }
-                    } else if (deviceInfoExt.getStartCheckMod() == null || "".equals(deviceInfoExt.getStartCheckMod())) {
-                        UiLogUtil.appendLog2EventTab(deviceCode, "没有设置开机check");
-                    }
-                }
-            }
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-            sqlSession.rollback();
-        } finally {
-            sqlSession.close();
-        }
-    }
-
     // </editor-fold> 
     // <editor-fold defaultstate="collapsed" desc="RemoteCommand">
-    @Override
-    public Map holdDevice() {
-        SqlSession sqlSession = MybatisSqlSession.getSqlSession();
-        DeviceService deviceService = new DeviceService(sqlSession);
-        DeviceInfoExt deviceInfoExt = deviceService.getDeviceInfoExtByDeviceCode(deviceCode);
-        sqlSession.close();
-        if (deviceInfoExt != null && "Y".equals(deviceInfoExt.getLockSwitch())) {
-            Map map = this.sendS2f41Cmd("STOP");//Map map = this.sendS2f41Cmd("LOCK");
-            if ((byte) map.get("HCACK") == 0 || (byte) map.get("HCACK") == 4) {
-                this.setAlarmState(2);
-            }
-            return map;
-        } else {
-            UiLogUtil.appendLog2EventTab(deviceCode, "未设置锁机！");
-            return null;
-        }
-    }
+//    @Override
+//    public Map holdDevice() {
+//        SqlSession sqlSession = MybatisSqlSession.getSqlSession();
+//        DeviceService deviceService = new DeviceService(sqlSession);
+//        DeviceInfoExt deviceInfoExt = deviceService.getDeviceInfoExtByDeviceCode(deviceCode);
+//        sqlSession.close();
+//        if (deviceInfoExt != null && "Y".equals(deviceInfoExt.getLockSwitch())) {
+//            Map map = this.sendS2f41Cmd("STOP");//Map map = this.sendS2f41Cmd("LOCK");
+//            if ((byte) map.get("HCACK") == 0 || (byte) map.get("HCACK") == 4) {
+//                this.setAlarmState(2);
+//            }
+//            return map;
+//        } else {
+//            UiLogUtil.appendLog2EventTab(deviceCode, "未设置锁机！");
+//            return null;
+//        }
+//    }
 
     //释放机台
     @Override
@@ -342,80 +205,32 @@ public class MVPHost extends EquipHost {
         return null;
     }
 
-    @Override
-    public Map startDevice() {
-        DataMsgMap s2f41out = new DataMsgMap("s2f41start", activeWrapper.getDeviceId());
-        s2f41out.setTransactionId(activeWrapper.getNextAvailableTransactionId());
-        DataMsgMap data = null;
-        try {
-            data = activeWrapper.sendAwaitMessage(s2f41out);
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-        byte[] hcack = (byte[]) ((SecsItem) data.get("HCACK")).getData();
-        Map resultMap = new HashMap();
-        resultMap.put("msgType", "s2f42");
-        resultMap.put("deviceCode", deviceCode);
-        resultMap.put("HCACK", hcack[0]);
-        return resultMap;
-    }
+//    @Override
+//    public Map startDevice() {
+//        DataMsgMap data = null;
+//        try {
+//            this.sendS2f41Cmd("START");
+//        } catch (Exception e) {
+//            logger.error("Exception:", e);
+//        }
+//        byte hcack = (byte) data.get("HCACK");
+//        Map resultMap = new HashMap();
+//        resultMap.put("msgType", "s2f42");
+//        resultMap.put("deviceCode", deviceCode);
+//        resultMap.put("HCACK", hcack);
+//        return resultMap;
+//    }
     // </editor-fold>
-
-
-    @SuppressWarnings("unchecked")
-    public void sendS2F33clear() {
-        DataMsgMap s2f37outAll = new DataMsgMap("s2f33clear", activeWrapper.getDeviceId());
-        long transactionId = activeWrapper.getNextAvailableTransactionId();
-        s2f37outAll.setTransactionId(transactionId);
-        try {
-            activeWrapper.sendAwaitMessage(s2f37outAll);
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void sendS2F35clear() {
-        DataMsgMap s2f37outAll = new DataMsgMap("s2f35clear", activeWrapper.getDeviceId());
-        long transactionId = activeWrapper.getNextAvailableTransactionId();
-        s2f37outAll.setTransactionId(transactionId);
-
-        try {
-            activeWrapper.sendAwaitMessage(s2f37outAll);
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-    }
-
-
-    private void sendS2f33outMulti(long reportId, long svid0, long svid1) {
-        DataMsgMap s2f33out = new DataMsgMap("s2f33outmulti", activeWrapper.getDeviceId());
-        s2f33out.setTransactionId(activeWrapper.getNextAvailableTransactionId());
-        long[] dataid = new long[1];
-        dataid[0] = reportId;
-        long[] reportid = new long[1];
-        reportid[0] = reportId;
-        long[] variableID0 = new long[1];
-        variableID0[0] = svid0;
-        long[] variableID1 = new long[1];
-        variableID1[0] = svid1;
-        long[] variableID2 = new long[1];
-        s2f33out.put("DataID", dataid);
-        s2f33out.put("ReportID", reportid);
-        s2f33out.put("VariableID0", variableID0);
-        s2f33out.put("VariableID1", variableID1);
-        try {
-            activeWrapper.sendAwaitMessage(s2f33out);
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-    }
 
     private void initRptPara() {
         sendS2F33clear();
         sendS2F35clear();
         //重新定义机台UpLoadStripMapping事件
-        sendS2f33outMulti(3014L, 1037L, 1023L);
+//        sendS2f33outMulti(3014L, 1037L, 1023L);
+        List list1 = new ArrayList();
+        list1.add(1037L);
+        list1.add(1023L);
+        sendS2F33Out(3014L,3014L,list1);
         sendS2F35out(3014L, 3014L, 3014L);
         sendS2F37out(3014L);
         logger.info("If the device software upgrades, you need to redefine the ceid=3043 event!！");
@@ -428,8 +243,8 @@ public class MVPHost extends EquipHost {
     }
 
 
-    @Override
-    public Map sendS7F5out(String recipeName) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+//    @Override
+//    public Map sendS7F5out(String recipeName) {
+//        throw new UnsupportedOperationException("Not supported yet.");
+//    }
 }
