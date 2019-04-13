9355df871684414dfc9e6d2e952c6f9fb3f0bff9
sunjx <sunjx@tfinfo.cn>
2019-04-13 14:23:53 +0800

    eap禁止关闭

--- src/main/java/cn/tzauto/octopus/gui/main/EapClient.java
+++ src/main/java/cn/tzauto/octopus/gui/main/EapClient.java
@@ -11,6 +11,7 @@ import cn.tzauto.octopus.common.mq.SubscribeMessage;
 import cn.tzauto.octopus.common.mq.common.MQConstants;
 import cn.tzauto.octopus.common.util.language.languageUtil;
 import cn.tzauto.octopus.common.util.tool.CommonUtil;
+import cn.tzauto.octopus.common.util.tool.dragUtil;
 import cn.tzauto.octopus.common.ws.InitService;
 import cn.tzauto.octopus.gui.EquipmentEventDealer;
 import cn.tzauto.octopus.gui.widget.equipstatuspane.EquipStatusPane;
@@ -24,6 +25,7 @@ import cn.tzauto.octopus.secsLayer.util.FengCeConstant;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -37,10 +39,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCharacterCombination;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
@@ -56,6 +58,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static java.awt.Color.red;
 
 /**
  * @author luosy
@@ -80,7 +84,7 @@ public class EapClient extends Application implements JobListener, PropertyChang
     public ConcurrentHashMap<String, EquipModel> equipModels;
     public static ConcurrentHashMap<String, EquipStatusPane> equipStatusPanes = new ConcurrentHashMap<>();
 
-    public static Parent root;
+    public static GridPane root;
     public Tab mainTab;
 
     public static ServerSocket server;
@@ -104,7 +108,6 @@ public class EapClient extends Application implements JobListener, PropertyChang
         stage.setMinHeight(600);
         Image image = new Image(getClass().getClassLoader().getResourceAsStream("logoTaiZhi.png"));
         stage.getIcons().add(image);
-
         window.initStyle(StageStyle.UNDECORATED);
         window.setResizable(false);
         Label loadinglabel = new Label();
@@ -125,12 +128,87 @@ public class EapClient extends Application implements JobListener, PropertyChang
         Platform.runLater(() -> {
             try {
                 ResourceBundle resourceBundle = ResourceBundle.getBundle("eap", new languageUtil().getLocale());//new Locale("zh", "TW");Locale.getDefault()
-                FXMLLoader fXMLLoader = new FXMLLoader();
                 root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"), resourceBundle);
-                stage.setTitle("EAPClient");
-                Scene scene = new Scene(root);
+                GridPane gridPane = new GridPane();
+                gridPane.setStyle("-fx-background-color: white;");
+                gridPane.setPrefHeight(32);
+                gridPane.setAlignment(Pos.CENTER_LEFT);
+                Label label = new Label("  EAPClient");
+                label.setFont(Font.font(14));
+                label.setTextFill(Paint.valueOf("BLACK"));
+                ImageView imageView = new ImageView("logoTaiZhi.png");
+                imageView.setFitHeight(24);
+                imageView.setFitWidth(24);
+                label.setGraphic(imageView);
+
+                Button minButton = new Button("—");
+                Button amxButton = new Button("口");
+                minButton.setFont(Font.font(14));
+                amxButton.setFont(Font.font(14));
+                VBox box = new VBox();
+                VBox vBox=new VBox();
+                minButton.setStyle("-fx-base: rgb(243,243,243);"
+                        + "-fx-max-height: infinity;-fx-text-fill: #000000 ; -fx-border-image-insets: 0;-fx-background-color: white;");
+                amxButton.setStyle("-fx-base: rgb(243,243,243); "
+                        + "-fx-max-height: infinity;-fx-text-fill: #000000 ; -fx-border-image-insets: 0;-fx-background-color: white");
+
+                minButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        minButton.setStyle("-fx-background-color: red");
+                    }
+                });
+                amxButton.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        amxButton.setStyle("-fx-background-color: red");
+                    }
+                });
+                minButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        minButton.setStyle("-fx-background-color: white");
+                    }
+                });
+                amxButton.setOnMouseExited(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        amxButton.setStyle("-fx-background-color: white");
+                    }
+                });
+                minButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        stage.setIconified(true);
+
+                    }
+                });
+                amxButton.setOnAction(new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent event) {
+                        stage.setMaximized(!stage.isMaximized());
+                        root.setPrefHeight(vBox.getHeight()-gridPane.getHeight());
+                    }
+                });
+
+                gridPane.addColumn(0, label);
+                gridPane.setHgrow(label, Priority.ALWAYS);
+                gridPane.addColumn(1, minButton);
+                gridPane.addColumn(2, amxButton);
+
+                vBox.getChildren().addAll(box,root);
+                // 拖动监听器
+                dragUtil.addDragListener(stage,gridPane);
+                // 添加窗体拉伸效果
+                dragUtil.addDrawFunc(stage, vBox,root);
+                box.getChildren().addAll(gridPane,root);
+
+                 stage.setTitle("EAPClient");
+                Scene scene = new Scene(vBox);
                 stage.setScene(scene);
-                stage.initStyle(StageStyle.DECORATED);
+                stage.initStyle(StageStyle.TRANSPARENT);
+
                 // String clientId = GlobalConstants.getProperty("clientId");
 //                stage.setFullScreen(true);
                 stage.setFullScreenExitHint("");
