d63358a82d56fe5253d385cdf4943a731ca85e21
leo <liqd@tfinfo.cn>
2019-07-15 14:13:39 +0800

    增加连接成功提示日志

--- src/main/java/cn/tzauto/octopus/gui/EquipmentEventDealer.java
+++ src/main/java/cn/tzauto/octopus/gui/EquipmentEventDealer.java
@@ -6,6 +6,7 @@ import cn.tzauto.octopus.gui.equipevent.CommFailureEvent;
 import cn.tzauto.octopus.gui.equipevent.CommStatusEvent;
 import cn.tzauto.octopus.gui.equipevent.ControlEvent;
 import cn.tzauto.octopus.gui.equipevent.ReceivedSeparateEvent;
+import cn.tzauto.octopus.gui.guiUtil.UiLogUtil;
 import cn.tzauto.octopus.gui.main.EapClient;
 import cn.tzauto.octopus.secsLayer.domain.EquipNodeBean;
 import cn.tzauto.octopus.secsLayer.domain.EquipState;
@@ -149,6 +150,7 @@ public class EquipmentEventDealer extends SwingWorker<Object, EquipState>
         logger.info("notificationOfJsipReady Invoked at device id " + deviceId + " equip name "
                 + equipNodeBean.getDeviceCode());
         eventQueue.add(new CommStatusEvent(true, deviceId));
+        UiLogUtil.getInstance().appendLog2EventTab(equipNodeBean.getDeviceCode(),"SECS连接正常启动...");
         stage.equipHosts.get(equipNodeBean.getDeviceCode()).setSdrReady(true);
         stage.equipHosts.get(equipNodeBean.getDeviceCode()).setIsRestarting(false);
         stage.equipHosts.get(equipNodeBean.getDeviceCode()).restartCnt = 0;
